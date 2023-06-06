902d240e9d83a19ef3f4a1e0b064ea9483687bb5
AnaClaraB2009 <123403593+AnaClaraB2009@users.noreply.github.com>
2023-06-06 18:25:43 -0300

    Add files via upload

--- Projeto 45-47/p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
+++ Projeto 45-47/p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
@@ -1,6 +1,42 @@
+var boatImg1;
+var boatImg2;
+var boatImg3;
+var boatImg4;
+var boatImg5;
+var titleImg;
+var seaImg; 
+var explosionImg;
+var gridImg;
+var bombImg; 
+
+var database;
+
+var boatGroup;
+
+function preLoad () {
+
+boatImg1 = loadImage ("./Images/navio 1.png")
+boatImg2 = loadImage ("./Images/navio 2.png")
+boatImg3 = loadImage ("./Images/navio 3.png")
+boatImg4 = loadImage ("./Images/navio 4.png")
+boatImg5 = loadImage ("./Images/navio 5.png")
+
+titleImg = loadImage ("./Images/titulo.png")
+seaImg = loadImage ("./Images/azul.png")
+gridImg = loadImage ("./Images/grid.png")
+
+explosionImg = loadImage ("./Images/explosão.png")
+bombImg = loadImage ("./Images/bomba.png")
+
+}
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+
+  boatGroup = new Group ()
+
+  database = firebase.database () 
+
 }
 
 function draw() {
